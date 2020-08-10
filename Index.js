9da85f04340d3d37bc50b2caefe98b43e04e9a23
Leo Ayala <leo_a1016@yahoo.com>
2020-08-09 20:50:59 -0400

    added HTML files and array of employees

--- Index.js
+++ Index.js
@@ -4,12 +4,12 @@ const Employee = require('./lib/Employee')
 const Manager = require('./lib/Manager')
 const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
-
 const inquirer = require("inquirer")
 
 var employees = []
 
-function promptUser() {
+function firstMessage() {
+    console.log('Please enter infromation about yourself')
     return inquirer.prompt([
         {
             type: 'input',
@@ -34,12 +34,90 @@ function promptUser() {
     ]);
 }
 
-promptUser()
+function makeEngineer() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your employee ID?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your Github name?'
+        }
+    ]);
+}
+
+function makeIntern() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your employee ID?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?'
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What is the name of your school?'
+        }
+    ]);
+}
+
+function teamMembers() {
+  console.log('Please enter information about your team members');
+    return inquirer.prompt(
+        {
+            type:'list',
+            name: 'type',
+            choices: ['Intern', 'Engineer', 'Done']
+        }
+    ) .then(choice => {
+        if (choice.type == 'Engineer') {
+            makeEngineer().then(answers => {
+                let engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+                employees.push(engineer)
+                teamMembers()
+            })
+        } else if (choice.type == 'Intern') {
+            makeIntern().then(answers => {
+                let intern = new Intern(answers.name, answers.id, answers.email, answers.school)
+                employees.push(intern)
+                teamMembers()
+            })
+        } else {
+
+        }
+    })
+}
+
+firstMessage()
     .then(answers => {
         let manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
         employees.push(manager);
     })
     .then(() => {
-        // getTeamData()
-        console.log(employees)
+         teamMembers()
     });
+
+module.exports = employees
--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,11 +1,31 @@
 const Employee = require('../lib/Employee')
 
 test('create an employee object', () => {
-    const employee = new Employee('Leo', 456, 'leo@leo.com');
+    const employee = new Employee('Leo', 456, 'leo@leo.com', 'Employee');
     
     expect(employee.name).toBe('Leo');
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
+});
+
+test('checks for a name', () => {
+    const employee = new Employee('Leo', 456, 'leo@leo.com', 'Employee');
+
+});
+
+test('checks for an id', () => {
+    const employee = new Employee('Leo', 456, 'leo@leo.com', 'Employee');
+
+});
+
+test('checks for an email', () => {
+    const employee = new Employee('Leo', 456, 'leo@leo.com', 'Employee');
+
+});
+
+test('checks for a role', () => {
+    const employee = new Employee('Leo', 456, 'leo@leo.com', 'Employee');
+
 });
 
 
